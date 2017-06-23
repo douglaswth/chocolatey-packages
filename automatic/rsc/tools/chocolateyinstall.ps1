8eb1e1f7c5a256d19cf4f6bdf081a6c06601005e
Douglas Thrift <douglas@douglasthrift.net>
2017-06-23 09:59:30 -0700

    Checkpoint progress on rsc package

---
+++ automatic/rsc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://binaries.rightscale.com/rsbin/rsc/v6.4.0/rsc-windows-amd64.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+  checksum64    = '24bf87d8e029f4da6b0e96611f3bdc4ba746f5de59c93896c9fb22d88458ecda'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
