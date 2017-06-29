4b7fc38672a82b120464f5ed25a10a48ba5669df
Douglas Thrift <douglas.thrift@rightscale.com>
2017-06-28 17:55:41 -0700

    Get automatic updates working for rsc package

--- automatic/rsc/tools/chocolateyinstall.ps1
+++ automatic/rsc/tools/chocolateyinstall.ps1
@@ -1,15 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= $env:ChocolateyPackageName
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://binaries.rightscale.com/rsbin/rsc/v6.4.0/rsc-windows-amd64.zip'
+$packageName = $env:ChocolateyPackageName
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url64bit      = $url64
-  checksum64    = '24bf87d8e029f4da6b0e96611f3bdc4ba746f5de59c93896c9fb22d88458ecda'
-  checksumType64= 'sha256'
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url64bit       = 'https://binaries.rightscale.com/rsbin/rsc/v6.4.1/rsc-windows-amd64.zip'
+  checksum64     = '6a953408725537a9ca2e75ddb594eb37e5e72d925d271b2e92bca2a664b4542a'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/rsc/update.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module au
+
+$branches = 'https://github.com/rightscale/rsc/branches/all'
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $branches
+  $regex         = '^v\d+\.\d+\.\d+$'
+  $branch        = $download_page.Links | ? innerText -Match $regex | select -First 1 -ExpandProperty innerText
+  $version       = $branch.Substring(1)
+  $url64         = "https://binaries.rightscale.com/rsbin/rsc/$branch/rsc-windows-amd64.zip"
+  return @{ Version = $version; URL64 = $url64 }
+}
+
+function global:au_SearchReplace {
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^\s+url64bit\s+=\s+)('.*')"       = "`$1'$($Latest.URL64)'"
+      "(^\s+checksum64\s+=\s+)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(^\s+checksumType64\s+=\s+)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+update -ChecksumFor 64
