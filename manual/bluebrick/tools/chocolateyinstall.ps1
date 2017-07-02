6929823242369a8e64ed983cda5d156581fd476c
Douglas Thrift <douglas@douglasthrift.net>
2017-07-01 22:14:58 -0700

    Package for BlueBrick LEGO Layout Editor

---
+++ manual/bluebrick/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = $env:ChocolateyPackageName
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$blueBrickExe = Join-Path $toolsDir 'BlueBrick.exe'
+$blueBrickLnk = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'BlueBrick.lnk'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://bluebrick.lswproject.com/download/BlueBrick1.8.1.zip'
+  checksum      = '1bce9fe3e3ff1595ddab07765aaa8232362e34a4a4d666dab5d0573eb4725f4a'
+  checksumType  = 'sha256'
+}
+$partsPackageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://bluebrick.lswproject.com/download/AllAdditionalParts.zip'
+  checksum      = 'f728151147d8c862ce88e50f1bf9cb2b748734f52417be8bb9c11fdf60f2150b'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage @partsPackageArgs
+Install-ChocolateyShortcut `
+  -ShortcutFilePath $blueBrickLnk `
+  -TargetPath $blueBrickExe `
+  -Description 'Plan LEGO® Layout with BlueBrick'
---
+++ manual/bluebrick/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$blueBrickLnk = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'BlueBrick.lnk'
+
+Remove-Item -Path $blueBrickLnk -Force | Out-Null
