c779e24ffd0870923c3f42dc15b6172ebc64a0fb
Douglas Thrift <douglas.thrift@rightscale.com>
2017-07-03 16:58:59 -0700

    Package for right_st

---
+++ automatic/right_st/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:ChocolateyPackageName
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url64bit       = 'https://binaries.rightscale.com/rsbin/right_st/v1.6.1/right_st-windows-amd64.zip'
+  checksum64     = 'e9757a64203634e8eeb887c08ab50672837f73b1d83dd6af0d5d94ed6dff11e7'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/right_st/update.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module au
+
+$latest_release = 'https://github.com/rightscale/right_st/releases/latest'
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $latest_release
+  $regex         = '^.+/v(\d+\.\d+\.\d+)/right_st-windows-amd64\.zip$'
+  $url64         = $download_page.Links | ? href -Match $regex | select -First 1 -ExpandProperty href
+  $version       = $url64 -replace $regex, '$1'
+  return @{ Version = $version; URL64 = $url64 }
+}
+
+function global:au_SearchReplace {
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^\s+url64bit\s+=\s+)('.*')"       = "`$1'$($Latest.URL64)'"
+      "(^\s+checksum64\s+=\s+)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(^\s+checksumType64\s+=\s+)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+update -ChecksumFor 64
