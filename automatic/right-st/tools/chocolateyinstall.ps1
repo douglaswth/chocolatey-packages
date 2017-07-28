e92f4dc9bb6f7cd9b2dcc29a511df12d2179f148
Chocolatey <chocolatey@realdimensions.net>
2017-07-28 00:09:15 +0000

    AU: 1 updated - right-st [skip ci] https://gist.github.com/d98014ed4fcaa5cc65bb902f21ce366a/3e6157145eca282b68bb2c5eb68c3e84857eaa44

--- automatic/right-st/tools/chocolateyinstall.ps1
+++ automatic/right-st/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://binaries.rightscale.com/rsbin/right_st/v1.6.1/right_st-windows-amd64.zip'
-  checksum64     = 'e9757a64203634e8eeb887c08ab50672837f73b1d83dd6af0d5d94ed6dff11e7'
+  url64bit       = 'https://binaries.rightscale.com/rsbin/right_st/v1.6.2/right_st-windows-amd64.zip'
+  checksum64     = '1d09c68dc6fec430e6cc3e4ab2fe49f1614c077d280b66ca349efaad2070ffaf'
   checksumType64 = 'sha256'
 }
 
