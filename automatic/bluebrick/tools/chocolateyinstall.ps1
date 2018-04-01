c122ecac792e7f9ea782f76b46cdeaa31c105eda
Douglas Thrift <douglas@douglasthrift.net>
2018-03-31 18:11:37 -0700

    Update BlueBrick to 1.8.2 since AppVeyor would not

--- automatic/bluebrick/tools/chocolateyinstall.ps1
+++ automatic/bluebrick/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $blueBrickLnk = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = 'https://bluebrick.lswproject.com/download/BlueBrick1.8.1.zip'        # BlueBrick
-  checksum      = '1bce9fe3e3ff1595ddab07765aaa8232362e34a4a4d666dab5d0573eb4725f4a'    # BlueBrick
+  url           = 'https://bluebrick.lswproject.com/download/BlueBrick1.8.2.zip'        # BlueBrick
+  checksum      = '37645dc4fe22193843b8b662eda983192eaf92b88f5cf0b9542311bbc0857ae7'    # BlueBrick
   checksumType  = 'sha256'                                                              # BlueBrick
 }
 $partsPackageArgs = @{
