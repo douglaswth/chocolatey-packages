c7a8fcacdcee629bf73537a4b13a74576181f6cd
Chocolatey <chocolatey@realdimensions.net>
2018-08-25 00:09:12 +0000

    AU: 1 updated - rsc [skip ci] https://gist.github.com/d98014ed4fcaa5cc65bb902f21ce366a/d370540e121d264d73306f16a83845f7257b2b1f

--- automatic/rsc/tools/chocolateyinstall.ps1
+++ automatic/rsc/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://binaries.rightscale.com/rsbin/rsc/v6.9.0/rsc-windows-amd64.zip'
-  checksum64     = '810eb404c4c83655e29ae4d3a9f8dd1dfccd8d3c9ef22d7a15602821bd533326'
+  url64bit       = 'https://binaries.rightscale.com/rsbin/rsc/v6.10.1/rsc-windows-amd64.zip'
+  checksum64     = '0b6736ab41ad98bb4769996b04b69c1c58c8b595487bb01c560a59aeb1c94e09'
   checksumType64 = 'sha256'
 }
 
