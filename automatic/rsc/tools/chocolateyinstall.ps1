92f65b2098c8d8677d1e5c41a91f7434f8f34bf3
Douglas Thrift <douglas@douglasthrift.net>
2019-06-07 21:44:04 -0700

    Update rsc to 7.0.0

--- automatic/rsc/tools/chocolateyinstall.ps1
+++ automatic/rsc/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://binaries.rightscale.com/rsbin/rsc/v6.11.0/rsc-windows-amd64.zip'
-  checksum64     = '6debdba725604c2288d55104511c43d3e6afaf4b049d38b93962d55bb0b54f4d'
+  url64bit       = 'https://binaries.rightscale.com/rsbin/rsc/v7.0.0/rsc-windows-amd64.zip'
+  checksum64     = 'b05e6194b1fa882c8f5141aef2db2a554bb01b17fd79fbb07c775f1b75609142'
   checksumType64 = 'sha256'
 }
 
