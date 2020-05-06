5e6c2814fb6d05cb6afc0f00af99faefef6342fd
Chocolatey <chocolatey@realdimensions.net>
2020-05-06 06:06:26 +0000

    AU: 1 updated - fpt [skip ci] https://gist.github.com/d98014ed4fcaa5cc65bb902f21ce366a/865874708db365941c823e85c4282d36a41f301a

--- automatic/fpt/tools/chocolateyinstall.ps1
+++ automatic/fpt/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://binaries.rightscale.com/rsbin/fpt/v1.0.8/fpt-windows-amd64.zip'
-  checksum64     = '0a68cd97aa281d86354071d9eea39e1881ab14f9f2a771c23ce876cbe80eab62'
+  url64bit       = 'https://binaries.rightscale.com/rsbin/fpt/v1.1.0/fpt-windows-amd64.zip'
+  checksum64     = 'b81f0d091ca1f0f0f6fa18affb23e4dad220bddaa61f39885f4299512f014f21'
   checksumType64 = 'sha256'
 }
 
