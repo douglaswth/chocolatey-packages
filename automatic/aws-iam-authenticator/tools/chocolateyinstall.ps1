1af164b074574f97b1b9983383397898cf7b856a
Chocolatey <chocolatey@realdimensions.net>
2026-08-27 08:40:28 +0000

    AU: 1 updated - aws-iam-authenticator [skip ci] https://gist.github.com/douglaswth/d98014ed4fcaa5cc65bb902f21ce366a/b62b34fb8e99a0f9a4538b8dc26682919dd6a861

--- automatic/aws-iam-authenticator/tools/chocolateyinstall.ps1
+++ automatic/aws-iam-authenticator/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = "$toolsDir\aws-iam-authenticator.exe"
-  url64bit       = 'https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.7.19/aws-iam-authenticator_0.7.19_windows_amd64.exe'
-  checksum64     = 'f6d280bcfb0b11b62f062ee0419aa11eeadef422bf7b4ec957aea88e6a76824a'
+  url64bit       = 'https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.7.20/aws-iam-authenticator_0.7.20_windows_amd64.exe'
+  checksum64     = '6bc77f1c0f1737c0367b46e399af83d561feeb837c8668dbfa07818e53639abb'
   checksumType64 = 'sha256'
 }
 
